21a81d18d04d61bee798feb58c8a52ed8b365225
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-10 08:39:46 +0000

    Create the read and write method in the  for the client type
    Created  the read and write method in the client.go file for the
    client type
    
    * read method - Allows us to read  from the client connection and
    send information to the room forward channel
    * write method - Allows us to send information from the send channel to the
    client connection

--- client.go
+++ client.go
@@ -18,3 +18,36 @@ type client struct{
 	room *room
 
 }
+
+
+// read method allows us to read from the client socket via the ReadMessage method
+
+func (c *client) read(){
+	defer c.socket.Close()
+	for{
+		_,msg,err := c.socket.ReadMessage()
+
+		// if the Read message encounters an error we break the loop
+		if err != nil{
+			return
+		}
+		//Messages from the client are sent to the rooms forward channel
+		c.room.forward <- msg
+	}
+}
+
+// The defer keyword to tidy up and close the web socket connection when the function returns
+
+// write method allows us to write to the client socket with data from the send channel
+
+func (c *client) write(){
+	defer c.socket.Close()
+	
+	for msg := range c.send{
+		err := c.socket.WriteMessage(websocket.TextMessage,msg)
+
+		if err!= nil{
+			return
+		}
+	}
+}
