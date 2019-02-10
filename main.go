973f09b330b75bb111a44d63edd790be50f5850a
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-10 07:57:22 +0000

    Create the  templateHandler type
    * Created the template handler type that allowa us to load template files  compile them and execute them

--- main.go
+++ main.go
@@ -3,21 +3,36 @@ package main
 import(
 	"log"
 	"net/http"
+	"text/template"
+	"path/filepath"
+	"sync"
+
 )
 
-func main(){
-	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte(`
-		<html>
-			<head>
-				<title> Chit Chat app </title>
-			</head>
-			<body>
-				Hello World
-			<//body>
-		</html>
-		`))
+
+type templateHandler struct{
+	once sync.Once
+	filename string
+	templ *template.Template // represents a single template
+}
+
+
+// this method will load the file compile the template and execute it
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	t.once.Do(func(){
+		// Compiling the template file
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",t.filename)))
 	})
+
+	// Executing the template file and write the output to the response writer method 
+	t.templ.Execute(w,nil)
+}
+
+
+func main(){
+	// We use the handle function that takes in a custom handler
+	http.Handle("/", &templateHandler{filename:"chat.html"})
+	//template handler is a valid http.Handler because of the ServerHttp
 
 	if err:= http.ListenAndServe(":8080",nil); err !=nil{
 		log.Fatal("ListenAndServe:",err)
