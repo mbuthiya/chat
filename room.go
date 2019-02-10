95c2c0e0136774d0acddd0eb537de082fc223771
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-10 09:31:33 +0000

    Create run method for the type room
    Created the run method for the room type to track any actions on the 3 room channels
    
    - When we recieve a message in the join channel we add a new client to the clients map with a value of true
    - When we recieve a message in the leave channel we delete a client from the  clients map
    - When we recieve a message i the forwards channel we loop through all the clients in the client map and we  add the message
    to the send channel for the client

--- room.go
+++ room.go
@@ -12,7 +12,33 @@ type room struct{
 	//Leave is a channel for clients wishing to leave the room
 	leave chan *client
 	
-	// clients hold all the current clients in the room
+	// join and leave only exist to safely remove clients from the client map
 
-	client map[*client]bool
+	// clients hold all the current clients in the room
+	clients map[*client]bool
+}
+
+func (r *room) run(){
+	for{
+		// We use select whenever we need to synchronize shared memory
+		// or take different actions depending on various activities within our channels
+		select{
+		case client := <-r.join:
+			//joining
+			r.clients[client] = true
+		case client := <- r.leave:
+			//leaving
+			delete(r.clients,client) // removing items from a maps
+			close(client.send)
+		case msg := <- r.forward:
+			//forward message to all clients
+			for client := range r.clients{
+				client.send <- msg
+			}
+		}
+
+	}
+
+	//The code will watch the 3 channels in our room and the select will 
+	//run the code  for that particular case
 }
