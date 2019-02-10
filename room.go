e0e62c217672983aa07a6088a42de82f9ebbfc94
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-10 08:44:30 +0000

    Update the room type to add 3 properties
    In the room type we have added 3  properties:
    *join a client channel that holds the client that want to join the room
    * leave is a client channel to hold the clients that want to leave the room
    * clients - a channel that holds all the clients in the room

--- room.go
+++ room.go
@@ -5,4 +5,14 @@ type room struct{
 	//that should be forwarded to the other clients
 
 	forward chan []byte
+
+	// Join is a channel for clients wishing to join the channel
+	join chan *client
+
+	//Leave is a channel for clients wishing to leave the room
+	leave chan *client
+	
+	// clients hold all the current clients in the room
+
+	client map[*client]bool
 }
