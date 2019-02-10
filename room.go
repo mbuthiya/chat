a147877c0ff5de7d1368f0da0d9addd01f96d05e
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-10 09:55:20 +0000

    Create a new room function and ServeHTTP method that allows us to create
    a new room and serve a new client

--- room.go
+++ room.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"net/http"
+	"log"
+	"github.com/gorilla/websocket"
+
+)
+
 type room struct{
 	// Forward is a channel that holds incoming messages
 	//that should be forwarded to the other clients
@@ -16,6 +23,17 @@ type room struct{
 
 	// clients hold all the current clients in the room
 	clients map[*client]bool
+}
+
+
+func newRoom() *room{
+
+	return &room{
+		forward: make(chan []byte),
+		join: make(chan *client),
+		leave: make(chan *client),
+		clients: make(map[*client]bool),
+	}
 }
 
 func (r *room) run(){
@@ -41,4 +59,31 @@ func (r *room) run(){
 
 	//The code will watch the 3 channels in our room and the select will 
 	//run the code  for that particular case
+}
+
+const(
+	socketBufferSize = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize:socketBufferSize,WriteBufferSize:socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter,req *http.Request){
+
+	socket, err :=upgrader.Upgrade(w,req,nil)
+	if err!=nil{
+		log.Fatal("ServeHTTP",err)
+		return
+	}
+
+	client := &client{
+		socket:socket,
+		send: make(chan []byte, messageBufferSize),
+		room: r,
+	}
+	r.join <- client
+	
+	defer func(){r.leave <- client}()
+	go client.write()
+	client.read()
 }
